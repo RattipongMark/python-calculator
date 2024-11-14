b4b44301c61354089b23a165132de85cb2195f2c
Rattipong Sakunjeen (Mark) <rattipong.mark@gmail.com>
2024-11-14 03:53:09 +0000

    Addtestcase and fixCalculator

--- calculator.py
+++ calculator.py
@@ -3,25 +3,66 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        temp_a = a
+        temp_b = b
+
+        if a < 0:
+            temp_a = -a
+        if b < 0:
+            temp_b = -b
+            
+        for i in range(temp_b):
+            result = self.add(result, temp_a)
+
+        if (a < 0 and b > 0) or (a > 0 and b < 0):
+            result = -result
+
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
+        temp_a = a
+        temp_b = b
+
+        if b == 0:
+            return "err"
+        if a < 0:
+            temp_a = -a
+        if b < 0:
+            temp_b = -b
+
+        while temp_a >= temp_b:
+            temp_a = self.subtract(temp_a, temp_b)
             result += 1
+
+        if (a < 0 and b > 0) or (a > 0 and b < 0):
+            result = -result
+
         return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
-        return a
+        result = 0
+        temp_a = a
+        temp_b = b
+
+        if b == 0:
+            return "err"
+        if a < 0:
+            temp_a = -a
+        if b < 0:
+            temp_b = -b
+        
+        while temp_a >= temp_b:
+            temp_a = temp_a - temp_b
+
+        if (a < 0 and b > 0) or (a > 0 and b < 0):
+            temp_a = -temp_a
+
+        return temp_a
 
 # Example usage:
 if __name__ == "__main__":
--- test_calculator.py
+++ test_calculator.py
@@ -6,10 +6,35 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
-    def test_add(self):
+    def test_add_positive_numbers(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
-    # Add the following test methods to the TestCalculator class:
+    def test_add_negative_and_zero(self):
+        self.assertEqual(self.calc.add(-1, 0), -1)
+
+    def test_sub_positive_numbers(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+
+    def test_sub_negative_number(self):
+        self.assertEqual(self.calc.subtract(-2, 1), -3)
+
+    def test_mul_positive_numbers(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+    
+    def test_mul_negative_number(self):
+        self.assertEqual(self.calc.multiply(2, -3), -6)
+
+    def test_divide_negative_number(self):
+        self.assertEqual(self.calc.divide(-4, 2), -2)
+    
+    def test_divide_by_zero(self):
+        self.assertEqual(self.calc.divide(4, 0), 'err')
+    
+    def test_mod_positive_numbers(self):
+        self.assertEqual(self.calc.modulo(5, -2), -1)
+    
+    def test_mod_by_zero(self):
+        self.assertEqual(self.calc.modulo(5, 0), 'err')
 
 if __name__ == '__main__':
     unittest.main()
